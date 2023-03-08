41ba269258dec0607d6d964af955cf785eb59d4e
Shayan <shayan.dev101@gmail.com>
2023-03-08 12:16:24 +0530

    design + donation footer

--- src/hooks/useAPI.tsx
+++ src/hooks/useAPI.tsx
@@ -1,23 +1,39 @@
+import {useState} from 'react';
 import { Geolocation } from '@capacitor/geolocation';
 
 
 const useApi = () =>{
 const url = 'http://api.aladhan.com/v1/timings';
+const [error, setError] = useState<string | null>(null);
 
 const school = 1; //
-  
 const getNemazTimes = async () =>{
 
+    
     const position = await Geolocation.getCurrentPosition();
     
     console.log(position);
 
-    const result = await fetch(`${url}/${new Date()}?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&school=${school}`);
+    try{
+        const result = await fetch(`${url}/${new Date()}?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&school=${school}`);
 
-    const data = await result.json();
-    console.log(data);
+        const data = await result.json();
+        console.log(data);
 
-    return data.data.timings
+        return data.data.timings
+    }
+    catch(e){
+        console.log(e);
+        // setError(e.message as string);
+        // alert(e.message as string);
+        if (typeof e === 'object' && e !== null && 'message' in e) {
+            setError(e.message as string);
+          } else {
+            setError('An unknown error occurred.');
+          }
+        alert(error);
+        console.log(error);
+    }
 
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonLabel } from '@ionic/react';
+import { IonButton,IonContent, IonFooter, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonCol, IonGrid, IonRow } from '@ionic/react';
 import React, { useState, useEffect } from 'react';
 import './Home.css';
+import logo from '../assets/icon/logo.png';
 import useApi from '../hooks/useAPI';
 
 
@@ -39,30 +40,62 @@ const Home: React.FC =  () => {
       setPrayerTimings(timings);
     }
     fetchPrayerTimings();
-  },[]);
+  },[]); //[] prevents continious infinite api firing. 
 
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Nemaz Time</IonTitle>
+        <IonTitle >
+          {/* <h4>Nemaz Time</h4>
+          <img src={logo} alt="Logo" style={{ width: '80px', height: 'auto', marginLeft: '10px' }}/> */}
+
+          <IonGrid>
+            <IonRow>
+              <IonCol size='9' className="align-left"><h4>Nemaz Time</h4></IonCol>
+              <IonCol size='3' className="align-right"><img src={logo} alt="Logo" style={{ width: '80px', height: 'auto' }}/></IonCol>              
+            </IonRow>
+          </IonGrid>
+        </IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent >
 
       
-      <IonList>
+      {/* <IonList>
           {Object.keys(prayerTimings).map((key) => (
             <IonItem key={key}>
-              <IonLabel>{key}</IonLabel>
-              <IonLabel>{prayerTimings[key as keyof PrayerTimings]}</IonLabel>
+              <IonLabel style={{ marginLeft: '10px', paddingLeft:'10px' }}>{key}</IonLabel>
+              <IonLabel style={{ marginLeft: '150px' }}>{prayerTimings[key as keyof PrayerTimings]}</IonLabel>
             </IonItem>
           ))}
-      </IonList>
+      </IonList> */}
     
+      <IonGrid>
+        {Object.keys(prayerTimings).map((key) => (
+          <IonRow  key={key}>
+            <IonCol size='6' className="align-left">{key}</IonCol>         
+            <IonCol size='6' className="align-right">{prayerTimings[key as keyof PrayerTimings]}</IonCol>
+          </IonRow>
+          ))}
+      </IonGrid>
 
       </IonContent>
+
+      <IonFooter>
+        <IonToolbar>
+      
+          <IonGrid>
+            <IonRow>
+              <IonCol size='8' className="align-left"><h4>Donate here:</h4></IonCol>
+              <IonCol size='4' className="align-right" style={{marginTop:'0em'}}><a href="https://rzp.io/l/hmBe4L5"><IonButton>Donate</IonButton></a></IonCol>              
+            </IonRow>
+          </IonGrid>
+      
+         
+        </IonToolbar>
+      </IonFooter>
     </IonPage>
   );
 };
