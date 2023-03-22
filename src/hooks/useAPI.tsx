bba14e8036dd8fe98cf4798eebdf5e3fd7830bab
Shayan <shayan.dev101@gmail.com>
2023-03-22 19:31:07 +0530

    alert if location/network unavailable

--- src/hooks/useAPI.tsx
+++ src/hooks/useAPI.tsx
@@ -1,46 +1,109 @@
 import {useState} from 'react';
 import { Geolocation } from '@capacitor/geolocation';
-import axios from "axios";
+import axios from 'axios';
+import { useIonAlert } from '@ionic/react';
+import { App } from '@capacitor/app';
+
+
 
 const useApi = () =>{
-const url = 'http://api.aladhan.com/v1/timings';
+const url = 'https://api.aladhan.com/v1/timings';
 const [error, setError] = useState<string | null>(null);
+const [presentAlert] = useIonAlert();
 
 const school = 1; //
+// const position ={coords:{ latitude:3434, longitude:989789}}
 const getNemazTimes = async () =>{
-
     
-    const position = await Geolocation.getCurrentPosition();
-    
-    console.log(position);
-
     try{
-        const data = await axios.get(`${url}/${new Date()}?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&school=${school}`,{
-            headers: {
-                'Content-Type': 'application/json',
-              }
+
+        const position = await Geolocation.getCurrentPosition();
+        console.log('pos:',position);
+        console.log('school:',school);
+
+        const result = await axios(`${url}/${new Date()}?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&school=${school}`,{
+            
+            method: "GET", // *GET, POST, PUT, DELETE, etc.
             // mode: "no-cors", // no-cors, *cors, same-origin
             
         });
-        // console.log("fetchResult:",result);
 
-
-        // const data = await result.json();
+        if(result==null){
+           
+        }
         
-        console.log("axiosData:",data.data);
+        
+        console.log("fetchJson:",result);
 
-        return data.data.data
+        return result.data.data
     }
     catch(e){
-        console.log(e);
-        // setError(e.message as string);
-        // alert(e.message as string);
+        console.error(e);
         if (typeof e === 'object' && e !== null && 'message' in e) {
-            setError(e.message as string);
-          } else {
-            setError('An unknown error occurred.');
-          }
-        console.log(error);
+
+            
+            if(e.message === 'User denied Geolocation' || 'location disabled')
+                {
+            
+                presentAlert({
+                    header: 'Alert!',
+                    subHeader: 'Important message',
+                    message: 'Location service/ GPS disabled, kindly enable and reopen the app.',
+                    buttons: [
+                      {
+                        text: 'Cancel',
+                        role: 'cancel',
+                        handler: () => {
+                            App.exitApp();
+
+                        },
+                      },
+                      {
+                        text: 'OK',
+                        role: 'confirm',
+                        handler: () => {
+                            App.exitApp();
+
+                        },
+                      },
+                    ],
+                  })
+
+            }
+            if(e.message === 'Network Error')
+                {
+            
+                presentAlert({
+                    header: 'Alert!',
+                    subHeader: 'Important message',
+                    message: 'Internet not available, kindly connect to internet then try again.',
+                    buttons: [
+                      {
+                        text: 'Cancel',
+                        role: 'cancel',
+                        handler: () => {
+                            App.exitApp();
+
+                        },
+                      },
+                      {
+                        text: 'OK',
+                        role: 'confirm',
+                        handler: () => {
+                            App.exitApp();
+
+                        },
+                      },
+                    ],
+                  })
+
+            }
+            
+            
+        setError(e.message as string);
+        } else {
+                setError('An unknown error occurred.');
+              }
         return error
     }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
-import {IonContent, IonPage,  
-           IonCol, IonGrid, IonRow, IonCard } from '@ionic/react';
+import {IonContent, IonPage,
+           IonCol, IonGrid, IonRow, IonCard, IonImg } from '@ionic/react';
 import React, { useState, useEffect } from 'react';
 import './Home.css';
 import useApi from '../hooks/useAPI';
@@ -23,7 +23,7 @@ interface PrayerTimings {
 }
 
 const Home: React.FC =  () => {
-  
+
   const {getNemazTimes} = useApi()
 
   const [prayerTimings, setPrayerTimings] = useState<PrayerTimings>({
@@ -39,12 +39,14 @@ const Home: React.FC =  () => {
     NisfShab:'',
     AkhiriPaher:'',
   });
+  const [engDate, setEngDate] = useState();
   const [hijriDate, setHijriDate] = useState([]);
   const [hijriDayEn, setHijriDayEn] = useState([]);
   const [hijriDayUrdu, setHijriDayUrdu] = useState([]);
   const [hijriMonthEn, setHijriMonthEn] = useState([]);
   const [hijriMonthUrdu, setHijriMonthUrdu] = useState([]);
-
+  const [hijriYear, setHijriYear] = useState([]);
+  const [hijriDayNumber, setHijriDayNumber] = useState([]);
 
   useEffect(() => {
     async function fetchPrayerTimings() {
@@ -55,22 +57,21 @@ const Home: React.FC =  () => {
           throw Error(`Fetch Failed`)
         }
         setPrayerTimings(data.timings);
-
+        setEngDate(data.date.readable);
+        setHijriDayNumber(data.date.hijri.day)
         setHijriDate(data.date.hijri.date);
-        
+        setHijriYear(data.date.hijri.year);
         setHijriDayEn(data.date.hijri.weekday.en);
         setHijriDayUrdu(data.date.hijri.weekday.ar);
-
+        
         setHijriMonthEn(data.date.hijri.month.en);
         setHijriMonthUrdu(data.date.hijri.month.ar);
 
       } catch (error) {
         console.error(error);
-
-        // App.reloadApp();
-        App.exitApp();
-        window.location.reload();
-        //Hard-Reload the app 
+        //App.exitApp();
+        // window.location.reload();
+        //Hard-Reload the app
       }
     }
     fetchPrayerTimings();
@@ -82,18 +83,24 @@ const Home: React.FC =  () => {
   return (
     <IonPage >
       <IonContent>
-        
-      <div className="headImage">  
+
+      {/* <IonRefresher slot="fixed" onIonRefresh={handleRefresh}>
+          <IonRefresherContent></IonRefresherContent>
+      </IonRefresher> */}
+
+
+      <div className="headImage">
         <center>
-          <h5>{hijriDate}</h5>
+          <h5>{engDate}</h5>
+          {/* <h5>{hijriDate}</h5> */}
+          <h5>{hijriDayNumber}-{hijriMonthUrdu}/{hijriMonthEn}-{hijriYear}</h5>
           <h5>{hijriDayEn}/{hijriDayUrdu}</h5>
-          <h5>{hijriMonthEn}/{hijriMonthUrdu}</h5>
         </center>
       </div>
 
 
 
-      
+
 
 
       <IonCard className="designImplementation">
@@ -105,16 +112,16 @@ const Home: React.FC =  () => {
                     ہمارا مقصد
           خالق کی عبادت اور مخلوق کی خدمت کرنا ہے
           </div>
-        
+
         <div className="marquee marqueeEnglish">
-          Hamara maqsad: Khaliq ki Ibadat Makhlook ki Khidmat. 
+          Hamara maqsad: Khaliq ki Ibadat Makhlook ki Khidmat.
         </div>
-        
+
       </div>
 
 
         <IonGrid className="rowSize">
-          
+
           <div className="specialRow">
 
             <IonRow className='designImplementation-specialRow-SehriEnd'>
@@ -140,13 +147,13 @@ const Home: React.FC =  () => {
           {Object.keys(prayerTimings).map((key) => (
 
             <IonRow className='designImplementation-row' key={key}>
-              <IonCol size='4' className="align-left">{key}</IonCol>         
+              <IonCol size='4' className="align-left">{key}</IonCol>
               <IonCol size='4' className="align-center">
                     {
-                      parseInt(prayerTimings[key as keyof PrayerTimings].split(':')[0]) >= 12 
+                      parseInt(prayerTimings[key as keyof PrayerTimings].split(':')[0]) >= 12
                       ? (parseInt(prayerTimings[key as keyof PrayerTimings].split(':')[0]) - 12) ===0 ? '12:'+ prayerTimings[key as keyof PrayerTimings].split(':')[1] + " PM"
                        : (parseInt(prayerTimings[key as keyof PrayerTimings].split(':')[0]) - 12) + ':'
-                      + prayerTimings[key as keyof PrayerTimings].split(':')[1] + " PM" 
+                      + prayerTimings[key as keyof PrayerTimings].split(':')[1] + " PM"
                       : prayerTimings[key as keyof PrayerTimings] + " AM"
                     }
               </IonCol>
@@ -154,16 +161,20 @@ const Home: React.FC =  () => {
             </IonRow>
           ))}
 
-          <IonRow className='invisible' >
-              <IonCol style={{height:"3.5rem"}}></IonCol> 
-          </IonRow>    
 
         </IonGrid>
+
+        <IonImg src="../assets/img/donationDetails.jpg" alt=""></IonImg>
+        
+        <IonRow className='invisible' >
+              <IonCol style={{height:"3.5rem"}}></IonCol>
+        </IonRow>
+
       </IonCard>
 
       </IonContent>
       <Footer/>
-      
+
     </IonPage>
   );
 };
